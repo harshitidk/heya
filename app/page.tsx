2998b7eed87106aa17bbc7de4e375dc3a949ae1a
Harshit Kumar Saidaniya <harshitheya@Harshits-MacBook-Air.local>
2026-03-07 02:57:10 +0530

    fix: reverted home background to absolute h-full to fit perfectly within entire page height on scrolled mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       </div>
 
       {/* Art Background SVG */}
-      <div className="fixed top-0 left-0 w-full h-screen z-0 overflow-hidden pointer-events-none">
+      <div className="absolute top-0 left-0 w-full h-full z-0 overflow-hidden pointer-events-none">
         <Image
           src={artBg}
           alt="Background artwork"
