760c1669fc99ef622921090947fd3370e067c925
Harshit Kumar Saidaniya <harshitheya@Harshits-MacBook-Air.local>
2026-03-18 00:08:05 +0530

    style: make watermark headers much larger using vw units to touch view width

--- components/WatermarkText.tsx
+++ components/WatermarkText.tsx
@@ -6,7 +6,7 @@ export function WatermarkText({ text, top, isDark }: { text: string; top?: boole
     return (
         <div className={`w-full flex items-center justify-center pointer-events-none select-none overflow-hidden z-0 flex flex-col justify-center ${top ? 'md:-mb-32' : 'my-12 md:my-32'}`}>
             <p
-                className={`font-bold text-[80px] md:text-[200px] lg:text-[280px] tracking-tighter lowercase leading-none opacity-[0.2] ${poppins.className} transition-all duration-700`}
+                className={`font-bold text-[28vw] md:text-[26vw] tracking-tighter lowercase leading-none opacity-[0.2] ${poppins.className} transition-all duration-700`}
                 style={{
                     backgroundImage: isDark ? "linear-gradient(to bottom, #6ad2ff, transparent)" : "linear-gradient(to bottom, #ff751f, transparent)",
                     color: "transparent",
