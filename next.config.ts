9a4d21dabea5f79e5be29d350f26dc965d07d197
Harshit Kumar Saidaniya <harshitheya@Harshits-MacBook-Air.local>
2026-03-21 19:07:53 +0530

    Correctly set basePath for production only to prevent local routing doubling

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/heya",
-  assetPrefix: "/heya/",
+  basePath: isProd ? "/heya" : "",
   images: {
     unoptimized: true,
   },
